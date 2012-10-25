b06c71387e403ecc6c0004569bf3d385baac2e7a
Kevin Como <kevincomo@gmail.com>
2012-10-25 17:24:17 +0800

    fix node task.js

--- Node/como/lib/task.js
+++ Node/como/lib/task.js
@@ -11,10 +11,11 @@ como.task(
 function task(steps){
 	this._steps = steps;
 	this._callback = null;
+	this.next = require('./core').bind(this._next, this);
 	return this;
 };
 
-task.prototype.next = function(err, result){
+task.prototype._next = function(err, result){
 	if(this._steps.length == 0) {
 	   if(this._callback) this._callback(err, result);
 	   return;
@@ -24,10 +25,10 @@ task.prototype.next = function(err, result){
 	try{
 		var _result = fn.apply(this, [err, result]);
 		if(_result != undefined){
-			this.next(null, _result);
+			this._next(null, _result);
 		}
 	} catch(e){
-	   this.next(e, null);
+	   this._next(e, null);
 	}
 };
 
@@ -45,7 +46,7 @@ task.prototype.skip = function(index){
 
 task.prototype.start = function(callback){
     this._callback = callback;
-    this.next(null, null);
+    this._next(null, null);
     return this;
 };
 
