e8947780fc632986159b3071e8bdfef3f1fa0254
Kevin Como <kevincomo@gmail.com>
2013-04-11 18:56:52 +0800

    xx

--- Plugin/editor/core.js
+++ Plugin/editor/core.js
@@ -105,6 +105,10 @@ Como.reg('editor/core.js', function(){
 			t: '移去格式',
 			pos: {w:22, h:22, x:0, y:0, ml:0, mt:0}
 		},
+		REMOVESTYLE: {
+			t: '移去样式',
+			pos: {w:22, h:22, x:0, y:0, ml:0, mt:0}
+		},
 		RESIZEADD: {
 			t: '扩大输入框',
 			pos: {w:22, h:22, x:-660, y:0, ml:0, mt:0}
@@ -167,7 +171,7 @@ Como.reg('editor/core.js', function(){
 		Colors: ["#000000","#000000","#000000","#003300","#006600","#009900","#00CC00","#00FF00","#330000","#333300","#336600","#339900","#33CC00","#33FF00","#660000","#663300","#666600","#669900","#66CC00","#66FF00","#000000","#333333","#000000","#000033","#003333","#006633","#009933","#00CC33","#00FF33","#330033","#333333","#336633","#339933","#33CC33","#33FF33","#660033","#663333","#666633","#669933","#66CC33","#66FF33","#000000","#666666","#000000","#000066","#003366","#006666","#009966","#00CC66","#00FF66","#330066","#333366","#336666","#339966","#33CC66","#33FF66","#660066","#663366","#666666","#669966","#66CC66","#66FF66","#000000","#999999","#000000","#000099","#003399","#006699","#009999","#00CC99","#00FF99","#330099","#333399","#336699","#339999","#33CC99","#33FF99","#660099","#663399","#666699","#669999","#66CC99","#66FF99","#000000","#CCCCCC","#000000","#0000CC","#0033CC","#0066CC","#0099CC","#00CCCC","#00FFCC","#3300CC","#3333CC","#3366CC","#3399CC","#33CCCC","#33FFCC","#6600CC","#6633CC","#6666CC","#6699CC","#66CCCC","#66FFCC","#000000","#FFFFFF","#000000","#0000FF","#0033FF","#0066FF","#0099FF","#00CCFF","#00FFFF","#3300FF","#3333FF","#3366FF","#3399FF","#33CCFF","#33FFFF","#6600FF","#6633FF","#6666FF","#6699FF","#66CCFF","#66FFFF","#000000","#FF0000","#000000","#990000","#993300","#996600","#999900","#99CC00","#99FF00","#CC0000","#CC3300","#CC6600","#CC9900","#CCCC00","#CCFF00","#FF0000","#FF3300","#FF6600","#FF9900","#FFCC00","#FFFF00","#000000","#00FF00","#000000","#990033","#993333","#996633","#999933","#99CC33","#99FF33","#CC0033","#CC3333","#CC6633","#CC9933","#CCCC33","#CCFF33","#FF0033","#FF3333","#FF6633","#FF9933","#FFCC33","#FFFF33","#000000","#0000FF","#000000","#990066","#993366","#996666","#999966","#99CC66","#99FF66","#CC0066","#CC3366","#CC6666","#CC9966","#CCCC66","#CCFF66","#FF0066","#FF3366","#FF6666","#FF9966","#FFCC66","#FFFF66","#000000","#FFFF00","#000000","#990099","#993399","#996699","#999999","#99CC99","#99FF99","#CC0099","#CC3399","#CC6699","#CC9999","#CCCC99","#CCFF99","#FF0099","#FF3399","#FF6699","#FF9999","#FFCC99","#FFFF99","#000000","#00FFFF","#000000","#9900CC","#9933CC","#9966CC","#9999CC","#99CCCC","#99FFCC","#CC00CC","#CC33CC","#CC66CC","#CC99CC","#CCCCCC","#CCFFCC","#FF00CC","#FF33CC","#FF66CC","#FF99CC","#FFCCCC","#FFFFCC","#000000","#FF00FF","#000000","#9900FF","#9933FF","#9966FF","#9999FF","#99CCFF","#99FFFF","#CC00FF","#CC33FF","#CC66FF","#CC99FF","#CCCCFF","#CCFFFF","#FF00FF","#FF33FF","#FF66FF","#FF99FF","#FFCCFF","#FFFFFF"],
 		Emots: ['0 0', '-30px 0','-60px 0', '-90px 0','-120px 0', '-150px 0','-180px 0', '-210px 0','-240px 0', '-270px 0','0 -30px', '-30px -30px','-60px -30px', '-90px -30px','-120px -30px', '-150px -30px','-180px -30px', '-210px -30px','-240px -30px', '-270px -30px','0 -60px', '-30px -60px','-60px -60px', '-90px -60px','-120px -60px', '-150px -60px','-180px -60px', '-210px -60px','-240px -60px', '-270px -60px','0 -90px', '-30px -90px','-60px -90px', '-90px -90px','-120px -90px', '-150px -90px','-180px -90px', '-210px -90px','-240px -90px', '-270px -90px'],
 		Style: {
-			'default': [new Array('CODE'), new Array('BOLD', 'ITALIC', 'UNDERLINE','FONT', 'SIZE', 'COLOR', 'BGCOLOR'), new Array('LINK', 'UNLINK','LEFT', 'CENTER', 'RIGHT', 'INDENT', 'OUTDENT'), new Array('ICON', 'IMG','SPECIAL')],
+			'default': [new Array('CODE'), new Array('BOLD', 'ITALIC', 'UNDERLINE','FONT', 'SIZE', 'COLOR', 'BGCOLOR'), new Array('LINK', 'UNLINK','LEFT', 'CENTER', 'RIGHT', 'INDENT', 'OUTDENT'), new Array('ICON', 'IMG','SPECIAL'), new Array('REMOVESTYLE')],
 			'simple': [new Array('BOLD', 'ITALIC', 'UNDERLINE','FONT', 'SIZE', 'COLOR', 'BGCOLOR'), new Array('LINK', 'UNLINK','LEFT', 'CENTER', 'RIGHT', 'INDENT', 'OUTDENT'), new Array('ICON', 'IMG','SPECIAL')]
 		}
 	};
@@ -554,6 +558,16 @@ Como.reg('editor/core.js', function(){
 			this.setValue();
 		},
 
+		_execute_removestyle: function(){
+			this.iframe.contentWindow.focus();
+			var doc = this.iframe.contentWindow.document;
+			var html = doc.body.innerHTML;
+			html = html.replace(/\ style="[\s\S]*?"/ig, '');
+			doc.body.innerHTML = html;
+			this.iframe.contentWindow.focus();
+			this.setValue();
+		},
+
 		_pannel: function(type, element){
 			this._pannelAllHide();
 			var ps = this.pannels;
@@ -910,6 +924,9 @@ Como.reg('editor/core.js', function(){
 					break;
 				case 'REMOVE':
 					this._execute('removeformat');
+					break;
+				case 'REMOVESTYLE':
+					this._execute_removestyle();
 					break;
 				case 'FONT':
 					this._pannel('FONT', el);
